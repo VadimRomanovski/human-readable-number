12d65e13886d84f50541ab4f92ded66e6a9670b2
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-03-20 10:38:10 +0300

    init: start human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
+let numbers = ["zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "ninteen"]
+let elevens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+let tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 module.exports = function toReadable (number) {
-  
+    let array = number.toString().split('');
+    let result = "";
+    if(array.length==3 && +(array[1]+array[2])>=10 && +(array[1]+array[2])<=19){
+        numbers.forEach((item, index) => {
+            if(+array[0] == index){
+                result+=item+" "+"hundred"
+            }
+        })
+        elevens.forEach((item, index) => {
+            if(+(array[1]+array[2])-10 == index){
+                result+=" "+item;
+            } 
+        })  
+    }else if(array.length==3){
+        
+        numbers.forEach((item, index) => {
+            if(+array[0] == index){
+                result+=item+" "+"hundred"
+            }
+        })
+        tens.forEach((item, index) => {
+            if(+array[1]-2 == index){
+                result+=" "+item;
+            } 
+        })
+        numbers.forEach((item, index) => {
+            if(+array[2] == index && +array[2] != 0){
+                result+=" "+item;
+            }
+        });
+         
+    }else if(array.length==2 && number>=10 && number<=19){
+        
+        elevens.forEach((item, index) => {
+            if(+(array[0]+array[1])-10 == index){
+                result+=item;
+            } 
+        })  
+    }else if(array.length==2 && number>=20){
+        
+        tens.forEach((item, index) => {
+            if(+array[0]-2 == index){
+                result+=item;
+            } 
+        })
+        numbers.forEach((item, index) => {
+            if(+array[1] == index && +array[1] != 0){
+                result+=" "+item;
+            }
+        });
+         
+    }else if(array.length==1){
+        numbers.forEach((item, index) => {
+            if(+array[0] == index){
+                result+=item;
+            }
+        })
+    }
+    return result
 }
